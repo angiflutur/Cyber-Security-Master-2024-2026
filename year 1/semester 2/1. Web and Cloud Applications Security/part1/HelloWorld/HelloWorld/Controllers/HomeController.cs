32eb9253abf36aa7fc3ae8223b345346ea97bde3
angiflutur <flutur.angelica@gmail.com>
2025-03-02 15:37:03 +0200

    HelloWorld

---
+++ year 1/semester 2/1. Web and Cloud Applications Security/part1/HelloWorld/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using System.Diagnostics;
+using HelloWorld.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+    }
+}
