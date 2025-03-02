c2fb08b91718f1f7663cf42cb8412b6dd8e492cf
angiflutur <flutur.angelica@gmail.com>
2025-03-02 19:55:40 +0200

    MVCStore - styling, partialViews

--- year 1/semester 2/1. Web and Cloud Applications Security/part1/MVCStore/MVCStore/Infrastructure/PageLinkTagHelper.cs
+++ year 1/semester 2/1. Web and Cloud Applications Security/part1/MVCStore/MVCStore/Infrastructure/PageLinkTagHelper.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using MVCStore.ViewModels;
 
@@ -20,6 +20,10 @@ namespace MVCStore.Infrastructure
         public ViewContext? ViewContext { get; set; }
         public PagingInfoViewModel? PageModel { get; set; }
         public string? PageAction { get; set; }
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = String.Empty;
+        public string PageClassNormal { get; set; } = String.Empty;
+        public string PageClassSelected { get; set; } = String.Empty;
         public override void Process(TagHelperContext context,
         TagHelperOutput output)
         {
@@ -30,9 +34,13 @@ namespace MVCStore.Infrastructure
                 for (int i = 1; i <= PageModel.TotalPages; i++)
                 {
                     TagBuilder tag = new TagBuilder("a");
-                    tag.Attributes["href"] = urlHelper.Action(PageAction,
-                    new { productPage = i });
-
+                    tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage
+                        ? PageClassSelected : PageClassNormal);
+                    }
                     tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
                 }
