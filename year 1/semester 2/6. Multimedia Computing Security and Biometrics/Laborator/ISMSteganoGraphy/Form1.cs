46601323ef64d6bfa6dc9bccf19801fe360be8a5
Angelica Flutur <94394449+angiflutur@users.noreply.github.com>
2025-07-27 12:17:37 +0300

    Add files via upload

---
+++ year 1/semester 2/6. Multimedia Computing Security and Biometrics/Laborator/ISMSteganoGraphy/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+
+namespace SteganoGraphy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHide_Click(object sender, System.EventArgs e)
+        {
+            Bitmap bitmap = (Bitmap)picImage.Image;
+            Stream messageStream = GetMessageStream();
+            if (messageStream.Length == 0)
+            {
+                MessageBox.Show("Please enter a message or select a file.");
+                txtMessageText.Focus();
+            }
+            else
+            {
+                Stream keyStream = GetKeyStream(true);
+                if (keyStream.Length == 0)
+                {
+                    MessageBox.Show("Please enter a password or select a key file.");
+                    txtKeyText.Focus();
+                }
+                else
+                {
+
+                    try
+                    {
+                        SteganoGraphy.HideMessageInBitmap(messageStream, bitmap, keyStream, chkGrayscale.Checked);
+                        picImage.Image = bitmap;
+                        SaveFileTo24RGB(fileName);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Exception:\r\n" + ex.Message);
+                    }
+                }
+                keyStream.Close();
+            }
+            messageStream.Close();
+            bitmap = null;
+        }
+
+        private void btnExtract_Click(object sender, System.EventArgs e)
+        {
+
+            Bitmap bitmap = (Bitmap)picImageExtract.Image;
+            Stream messageStream = new MemoryStream();
+            Stream keyStream = GetKeyStream(false);
+            if (keyStream.Length == 0)
+            {
+                MessageBox.Show("Please enter a password or select a key file.");
+                txtKeyTextExtract.Focus();
+            }
+            else
+            {
+
+                try
+                {
+                    SteganoGraphy.ExtractMessageFromBitmap(bitmap, keyStream, ref messageStream);
+                    messageStream.Seek(0, SeekOrigin.Begin);
+                    StreamReader reader = new StreamReader(messageStream, UnicodeEncoding.UTF8);
+                    String readerContent = reader.ReadToEnd();
+                    if (readerContent.Length > txtExtractedMsgText.MaxLength)
+                    {
+                        readerContent = readerContent.Substring(0, txtExtractedMsgText.MaxLength);
+                    }
+                    txtExtractedMsgText.Text = readerContent;
+                    this.SuspendLayout();
+                    picImageExtract.Image.Dispose();
+                    picImageExtract.Image = null;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Exception:\r\n" + ex.Message);
+                }
+            }
+            messageStream.Close();
+            keyStream.Close();
+            bitmap = null;
+        }
+
+        private Stream GetMessageStream()
+        {
+            Stream messageStream;
+            byte[] messageBytes = UnicodeEncoding.UTF8.GetBytes(txtMessageText.Text);
+            messageStream = new MemoryStream(messageBytes);
+            return messageStream;
+        }
+
+        private Stream GetKeyStream(bool endec)
+        {
+            Stream keyStream;
+            byte[] keyBytes;
+            if (endec)
+                keyBytes = UnicodeEncoding.UTF8.GetBytes(txtKeyText.Text);
+            else
+                keyBytes = UnicodeEncoding.UTF8.GetBytes(txtKeyTextExtract.Text);
+            keyStream = new MemoryStream(keyBytes);
+            return keyStream;
+        }
+
+        private void SetImage(String fileName, bool action)
+        {
+            if (action)
+                picImage.Image = new Bitmap(fileName);
+            else
+                picImageExtract.Image = new Bitmap(fileName);
+            btnHide.Enabled = btnExtract.Enabled = true;
+        }
+
+        private String GetFileName(String filter)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Multiselect = false;
+            if (filter.Length > 0) { dlg.Filter = filter; }
+
+            if (dlg.ShowDialog(this) != DialogResult.Cancel)
+            {
+                return dlg.FileName;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void btnImageFile_Click(object sender, System.EventArgs e)
+        {
+            fileName = GetFileName("BitmapFile (*.bmp)|*.bmp");
+            if (fileName != null)
+            {
+                if (hiex)
+                    txtImageFile.Text = fileName;
+                else
+                    txtImageFileExtract.Text = fileName;
+                SetImage(fileName, hiex);
+            }
+        }
+
+        private void SaveFileTo24RGB(string file)
+        {
+            Bitmap img = new Bitmap(picImage.Image);
+            this.SuspendLayout();
+            picImage.Image.Dispose();
+            picImage.Image = null;
+            btnHide.Enabled = btnExtract.Enabled = false;
+            txtImageFile.Text = String.Empty;
+            this.ResumeLayout();
+            char[] trimChars = { '.', 'b', 'm', 'p' };
+            string fileShort = fileName.TrimEnd(trimChars);
+            //transforming 32 in 24 color bits image
+            Bitmap bitmap = new Bitmap(img.Width, img.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color temp = img.GetPixel(i, j);
+                    bitmap.SetPixel(i, j, temp);
+                }
+            } //low performance
+            img.Save(fileShort + "_encoded.bmp", System.Drawing.Imaging.ImageFormat.Bmp);
+        }
+
+        private void txtImageFile_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SetImage(txtImageFile.Text, true);
+            }
+        }
+
+        private void tabControl2_Selecting(object sender, TabControlCancelEventArgs e)
+        {
+            if (tabControl2.SelectedTab.Text == "Encoding")
+                hiex = true;
+            else
+                hiex = false;
+            if (tabControl2.SelectedTab.Text == "Decoding")
+            {
+                txtExtractedMsgText.Text = "";
+                txtKeyTextExtract.Text = "";
+            }
+        }
+
+        private void hideToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tabControl2.SelectedIndex = 0;
+            hiex = true;
+        }
+
+        private void extractToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tabControl2.SelectedIndex = 1;
+            hiex = false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ year 1/semester 2/6. Multimedia Computing Security and Biometrics/Laborator/ISMSteganoGraphy/SteganoGraphy.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Text;
+using System.IO;
+
+namespace SteganoGraphy
+{
+    public class SteganoGraphy
+    {
+        public static void HideMessageInBitmap(Stream messageStream, Bitmap bitmap, Stream keyStream, bool useGrayscale)
+        {
+            HideOrExtract(ref messageStream, bitmap, keyStream, false, useGrayscale);
+            messageStream = null;
+        }
+
+        public static void ExtractMessageFromBitmap(Bitmap bitmap, Stream keyStream, ref Stream messageStream)
+        {
+            HideOrExtract(ref messageStream, bitmap, keyStream, true, false);
+        }
+
+        private static void HideOrExtract(ref Stream messageStream, Bitmap bitmap, Stream keyStream, bool extract, bool useGrayscale)
+        {
+            Point pixelPosition = new Point(1, 0);
+
+            int currentStepWidth = 0;
+            byte currentKeyByte;
+            byte currentReverseKeyByte;
+            long keyPosition;
+            int dispersion = 1;
+            int bitmapWidth = bitmap.Width;
+            int bitmapHeight = bitmap.Height;
+            int currentColor = 0;
+            Color pixelColor;
+            Int32 messageLength;
+
+            //decode init
+            if (extract)
+            {
+                keyStream.Seek(0, SeekOrigin.Begin);
+                pixelColor = bitmap.GetPixel(0, 0);
+                messageLength = (pixelColor.R << 2) + (pixelColor.G << 1) + pixelColor.B;
+                long countPixels = (bitmapWidth * bitmapHeight) - 1;
+                long countRequiredPixels = 1;
+                while ((keyStream.Position < keyStream.Length) && (keyStream.Position < messageLength))
+                {
+                    countRequiredPixels += keyStream.ReadByte();
+                }
+                countRequiredPixels *= (long)System.Math.Ceiling(((float)messageLength / (float)keyStream.Length));
+                dispersion = (int)Math.Truncate(((float)countPixels / (float)countRequiredPixels));
+                messageStream = new MemoryStream(messageLength);
+            }
+            else ///encode init
+            {
+
+                messageLength = (Int32)messageStream.Length;
+
+                if (messageStream.Length >= 16777215) // 3B RGB
+                {
+                    String exceptionMessage = "Message too long,  must be less than 2^24 bytes.";
+                    throw new Exception(exceptionMessage);
+                }
+                long countPixels = (bitmapWidth * bitmapHeight) - 1;
+                long countRequiredPixels = 1;
+                while ((keyStream.Position < keyStream.Length) && (keyStream.Position < messageLength))
+                {
+                    countRequiredPixels += keyStream.ReadByte();
+                }
+                countRequiredPixels *= (long)System.Math.Ceiling(((float)messageStream.Length / (float)keyStream.Length));
+                dispersion = (int)Math.Truncate(((float)countPixels / (float)countRequiredPixels));
+                if (countRequiredPixels > countPixels)
+                {
+                    String exceptionMessage = "Image too small, must have at least " + countRequiredPixels + " pixels.";
+                    throw new Exception(exceptionMessage);
+                }
+                //encoding the length of the message into the first 0,0 pixel coordinates
+                int colorValue = messageLength;
+                int red = colorValue >> 2;
+                colorValue -= red << 2;
+                int green = colorValue >> 1;
+                int blue = colorValue - (green << 1);
+                pixelColor = Color.FromArgb(red, green, blue);
+                bitmap.SetPixel(0, 0, pixelColor);
+            }
+
+            keyStream.Seek(0, SeekOrigin.Begin);
+            messageStream.Seek(0, SeekOrigin.Begin);
+
+
+            for (int messageIndex = 0; messageIndex < messageLength; messageIndex++)
+            {
+                //calculate the pixel position x,y by
+                //reusing the key - reinitialize the key
+                if (keyStream.Position == keyStream.Length)
+                {
+                    keyStream.Seek(0, SeekOrigin.Begin);
+                }
+
+                currentKeyByte = (byte)keyStream.ReadByte();
+                currentStepWidth = (currentKeyByte == 0) ? (byte)1 : currentKeyByte * dispersion;
+
+                keyPosition = keyStream.Position;
+                keyStream.Seek(-keyPosition, SeekOrigin.End);
+                currentReverseKeyByte = (byte)keyStream.ReadByte();
+                keyStream.Seek(keyPosition, SeekOrigin.Begin);
+
+                while (currentStepWidth > bitmapWidth)
+                {
+                    currentStepWidth -= bitmapWidth;
+                    pixelPosition.Y++;
+                }
+                if ((bitmapWidth - pixelPosition.X) <= currentStepWidth)
+                {
+                    pixelPosition.X = currentStepWidth - (bitmapWidth - pixelPosition.X);
+                    pixelPosition.Y++;
+                }
+                else
+                {
+                    pixelPosition.X += currentStepWidth;
+                }
+                pixelColor = bitmap.GetPixel(pixelPosition.X, pixelPosition.Y);
+                //this is where the actual encoding/decoding phase is starting
+                if (extract)
+                {
+                    byte currentByte = (byte)(currentReverseKeyByte ^ GetColorComponent(pixelColor, currentColor));
+                    messageStream.WriteByte(currentByte);
+                    //    currentColor = (currentColor == 2) ? 0 : (currentColor + 1);
+                }
+                else
+                {
+                    //the byte that needs to be encoded
+                    int currentByte = messageStream.ReadByte() ^ currentReverseKeyByte;
+                    if (useGrayscale)
+                    {
+                        pixelColor = Color.FromArgb(currentByte, currentByte, currentByte);
+                    }
+                    else
+                    {
+                        SetColorComponent(ref pixelColor, currentColor, currentByte);
+                        //        currentColor = (currentColor == 2) ? 0 : (currentColor + 1);
+                    }
+
+                    bitmap.SetPixel(pixelPosition.X, pixelPosition.Y, pixelColor);
+
+
+                }
+                //else
+                //{
+                //    //the byte that needs to be encoded
+                //    int currentByte = messageStream.ReadByte() ^ currentReverseKeyByte;
+
+                //    byte r = pixelColor.R;
+                //    byte g = pixelColor.G;
+                //    byte b = pixelColor.B;
+
+                //    switch (currentColor)
+                //    {
+                //        case 0:
+                //            r = (byte)currentByte; break;
+                //        case 1:
+                //            g = (byte)currentByte; break;
+                //        case 2:
+                //            b = (byte)currentByte; break;
+                //    }
+                //    pixelColor = Color.FromArgb(r, g, b);
+                //    bitmap.SetPixel(pixelPosition.X, pixelPosition.Y, pixelColor);
+                //}
+                currentColor = (currentColor == 2) ? 0 : (currentColor + 1);
+            }
+            bitmap = null;
+            keyStream = null;
+
+        }
+
+        private static byte GetColorComponent(Color pixelColor, int colorComponent)
+        {
+            byte returnValue = 0;
+            switch (colorComponent)
+            {
+                case 0:
+                    returnValue = pixelColor.R;
+                    break;
+                case 1:
+                    returnValue = pixelColor.G;
+                    break;
+                case 2:
+                    returnValue = pixelColor.B;
+                    break;
+            }
+            return returnValue;
+        }
+
+        private static void SetColorComponent(ref Color pixelColor, int colorComponent, int newValue)
+        {
+            switch (colorComponent)
+            {
+                case 0:
+                    pixelColor = Color.FromArgb(newValue, pixelColor.G, pixelColor.B);
+                    break;
+                case 1:
+                    pixelColor = Color.FromArgb(pixelColor.R, newValue, pixelColor.B);
+                    break;
+                case 2:
+                    pixelColor = Color.FromArgb(pixelColor.R, pixelColor.G, newValue);
+                    break;
+            }
+        }
+    }
+}
